a5b109b00f3ca237e10c9deafb0fd28cd922782e
Ahmad44452 <88369569+Ahmad44452@users.noreply.github.com>
2023-06-05 19:28:36 +0500

    Update server.js

--- site/server/server.js
+++ site/server/server.js
@@ -1,7 +1,6 @@
-const express = require('express')
-// const { PythonShell } = require('python-shell');
+const express = require('express');
 const { spawn } = require('child_process');
-const app = express()
+const app = express();
 const port = 3001;
 const cors = require('cors');
 
@@ -28,7 +27,6 @@ app.get('/:age/:estimatedSalary', (req, res) => {
     // in close event we are sure that stream from child process is closed
     python.on('close', (code) => {
       console.log(`child process close all stdio with code ${code}`);
-      // send data to browser
       res.status(200).json(dataToSend)
     });
 
